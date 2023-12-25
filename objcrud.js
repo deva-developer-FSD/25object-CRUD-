fb66bef76977d87b0f0cfd39ff2b3e087e0cb8ce
Devendhran S <103272706+deva-developer-FSD@users.noreply.github.com>
2023-12-25 09:04:16 -0800

    Add files via upload

---
+++ objcrud.js
@@ -0,0 +1,17 @@
+var details={
+    name:`deva`,
+    age:23,
+    year:2001,
+    add:`trichy`,
+};
+
+//details.gender=`male`;
+details.attentence=[`mon`,`thus`,`wed`];
+details.age=`21`;
+//delete details.add
+console.log(details);
+//details.attentence=[`mon`,`thus`,`wed`];
+for(i=0; i<details.attentence.length;i++)
+console.log(details.attentence[i]);
+
+    
